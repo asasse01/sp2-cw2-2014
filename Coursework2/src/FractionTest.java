5212e1f193d7d0219d44f4922102d31ead5116d1
= <sassela@live.com>
2014-10-18 18:44:20 +0100

    Fraction class

---
+++ Coursework2/src/FractionTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author abby
+ *
+ */
+public class FractionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
