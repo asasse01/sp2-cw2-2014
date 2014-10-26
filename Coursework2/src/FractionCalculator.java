54958875168323841c5c5d5065206e87b8418ee5
= <sassela@live.com>
2014-10-26 13:09:08 +0000

    absolute, clear and negate actions in evaluate method

--- Coursework2/src/FractionCalculator.java
+++ Coursework2/src/FractionCalculator.java
@@ -6,33 +6,43 @@ public class FractionCalculator {
 
 	}
 	
-	/*
-	private Fraction evaluate(Fraction fraction, String input) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public static Fraction evaluate(Fraction fraction, String input) {
+		// recursive function to break string?
+		Fraction valueinCalc = new Fraction(0, 1);
+		String stored;
+		//valueinCalc = fraction;
+		if (input == "a" || input == "abs" || input == "A")  {
+			valueinCalc = fraction.getAbsVal();
+			//clear  valueinCalc?
+		} else if (input == "n" || input == "neg" || input == "N")  {
+			valueinCalc = fraction.negate();
+			//clear  valueinCalc?
+		} else if (input == "c" || input == "clear" || input == "C")  {
+			valueinCalc = new Fraction(0, 1);
+			//clear  valueinCalc?
+		} 
+		
+		Fraction result = valueinCalc;
+
+		return result;
 	}
-	*/
+	
 	
 	public static Fraction toFraction(String str) {
-		
+		//method converts string to fraction
 		int numDigits = 0;
 		int numEnd = str.length();
 		
-		for (int i = 0; str.charAt(i) != '/'; i++) {
-			//if (str.charAt(i) != '/') {
-				numDigits++;
-			//} 
-			/*else  {
-				numEnd = numDigits + 1;
-				break;
-			}*/ 
+		for (int i = 0; (str.charAt(i) != '/'); i++) {
+			numDigits++;
 		}
 		numEnd = numDigits;
 		
 		int num = Integer.parseInt(str.substring(0, numEnd));
 		int denom = Integer.parseInt(str.substring(numEnd+1));
 
-		Fraction result = new Fraction (num, denom);
+		Fraction result = new Fraction(num, denom);
 				
 		return result;
 	}
--- Coursework2/src/FractionCalculatorTest.java
+++ Coursework2/src/FractionCalculatorTest.java
@@ -5,26 +5,26 @@ import org.junit.Test;
 
 public class FractionCalculatorTest {
 
-	/*
+	
 	@Test
 	public void testEvaluate() {
-		assertEquals((new Fraction(3, 10)), evaluate((new Fraction(3, 5)), "* 1/2"));
-		assertEquals((new Fraction(1, 10)), evaluate((new Fraction(3, 10)), "+ -2/10"));
-		assertEquals((new Fraction(-3, 4)), evaluate((new Fraction(1, 6)), "- 11/12"));
-		assertEquals((new Fraction(2, 5)), evaluate((new Fraction(8, 10)), "/ 4/ 2"));
+		//assertEquals((new Fraction(3, 10)), evaluate((new Fraction(3, 5)), "* 1/2"));
+		//assertEquals((new Fraction(1, 10)), evaluate((new Fraction(3, 10)), "+ -2/10"));
+		//assertEquals((new Fraction(-3, 4)), evaluate((new Fraction(1, 6)), "- 11/12"));
+		//assertEquals((new Fraction(2, 5)), evaluate((new Fraction(8, 10)), "/ 4/ 2"));
 		
-		assertEquals((new Fraction(2, 6)), evaluate((new Fraction(-2, 6)), "a"));
-		assertEquals((new Fraction(7, -10)), evaluate((new Fraction(7, 10)), "neg"));
-		assertEquals((0), evaluate((new Fraction(7, 10)), "C"));
+		//assertEquals((new Fraction(2, 6)), FractionCalculator.evaluate((new Fraction(-2, 6)), "a"));
+		//assertEquals((new Fraction(7, -10)), FractionCalculator.evaluate((new Fraction(7, 10)), "neg"));
+		assertEquals((new Fraction(0, 1)), FractionCalculator.evaluate((new Fraction(7, 10)), "C"));
 
-	 }*/
+	 }
 
 	@Test
 	public void testToFraction() {
-		assertEquals((new Fraction(3, 10)), toFraction("3/10"));
-		assertEquals((new Fraction(1, 10)), toFraction("2/20"));
-		assertEquals((new Fraction(-3, 4)), toFraction("-3/4"));
-		assertEquals((new Fraction(2, 5)), toFraction("-2/-5"));
+		assertEquals((new Fraction(3, 10)), FractionCalculator.toFraction("3/10"));
+		assertEquals((new Fraction(1, 10)), FractionCalculator.toFraction("2/20"));
+		assertEquals((new Fraction(-3, 4)), FractionCalculator.toFraction("-3/4"));
+		assertEquals((new Fraction(2, 5)), FractionCalculator.toFraction("-2/-5"));
 	}
 
 }
