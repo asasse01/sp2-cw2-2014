6154ed23e5b3ecf9b952fb87ac0be125055d61d2
= <sassela@live.com>
2014-10-31 15:57:24 +0000

    evaluate test comments added

--- Coursework2/src/FractionCalculatorTest.java
+++ Coursework2/src/FractionCalculatorTest.java
@@ -8,13 +8,13 @@ public class FractionCalculatorTest {
 	
 	@Test
 	public void testEvaluate() {
-		//assertEquals((new Fraction(3, 10)), evaluate((new Fraction(3, 5)), "* 1/2"));
-		//assertEquals((new Fraction(1, 10)), evaluate((new Fraction(3, 10)), "+ -2/10"));
-		//assertEquals((new Fraction(-3, 4)), evaluate((new Fraction(1, 6)), "- 11/12"));
-		//assertEquals((new Fraction(2, 5)), evaluate((new Fraction(8, 10)), "/ 4/ 2"));
+		assertEquals((new Fraction(3, 10)), FractionCalculator.evaluate((new Fraction(3, 5)), "* 1/2"));
+		assertEquals((new Fraction(1, 10)), FractionCalculator.evaluate((new Fraction(3, 10)), "+ -2/10"));
+		assertEquals((new Fraction(-3, 4)), FractionCalculator.evaluate((new Fraction(1, 6)), "- 11/12"));
+		assertEquals((new Fraction(2, 5)), FractionCalculator.evaluate((new Fraction(8, 10)), "/ 4/2"));
 		
-		//assertEquals((new Fraction(2, 6)), FractionCalculator.evaluate((new Fraction(-2, 6)), "a"));
-		//assertEquals((new Fraction(7, -10)), FractionCalculator.evaluate((new Fraction(7, 10)), "neg"));
+		assertEquals((new Fraction(2, 6)), FractionCalculator.evaluate((new Fraction(-2, 6)), "a"));
+		assertEquals((new Fraction(7, -10)), FractionCalculator.evaluate((new Fraction(7, 10)), "neg"));
 		assertEquals((new Fraction(0, 1)), FractionCalculator.evaluate((new Fraction(7, 10)), "C"));
 
 	 }
