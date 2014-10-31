7fdf1595fad14a4a346b3c6437c0bb1ab09fa331
= <sassela@live.com>
2014-10-31 15:58:41 +0000

    evaluate method calculates single fraction operation

--- Coursework2/src/FractionCalculator.java
+++ Coursework2/src/FractionCalculator.java
@@ -8,11 +8,16 @@ public class FractionCalculator {
 	
 	
 	public static Fraction evaluate(Fraction fraction, String input) {
-		// recursive function to break string?
+		// TODO recursive function to break string?
+		// TODO convert if to switch statements
 		Fraction valueinCalc = new Fraction(0, 1);
-		String stored;
-		//valueinCalc = fraction;
-		if (input == "a" || input == "abs" || input == "A")  {
+		char stored = 0;
+		String fractionSubstr = "";
+		String inputchar;
+		int split = 0;
+		
+		if (input == "a" || input == "abs" || input == "A" )  {
+			// shorten if statement
 			valueinCalc = fraction.getAbsVal();
 			//clear  valueinCalc?
 		} else if (input == "n" || input == "neg" || input == "N")  {
@@ -20,10 +25,40 @@ public class FractionCalculator {
 			//clear  valueinCalc?
 		} else if (input == "c" || input == "clear" || input == "C")  {
 			valueinCalc = new Fraction(0, 1);
-			//clear  valueinCalc?
-		} 
+		} else {
+			for (int i = 0; i < input.length()-1; i++) {
+				
+				inputchar = input.substring(i, i+1);
+				//TODO new function to continue once fraction operated on
+				if (input.charAt(i) == '+' || input.charAt(i) == '-' 
+						|| input.charAt(i) == '*' 
+						|| input.charAt(i) == '/') {
+					//raise exception if already stored
+					stored = input.charAt(i); 
+				} else if (inputchar.equals(" "))  {
+					split = i;
+					break;
+				}
+			}
+			
+			for (int j = split+1; j < input.length(); j++) { 
+				fractionSubstr += input.substring(j, j+1);
+			}
+			
+			Fraction inputFrac = toFraction(fractionSubstr);
+			
+			if (stored == '+') {
+			valueinCalc = fraction.add(inputFrac);
+			} else if (stored == '-') {
+				valueinCalc = fraction.subtract(inputFrac);
+			} else if (stored == '*') {
+				valueinCalc = fraction.multiply(inputFrac);
+			} else if (stored == '/') {
+				valueinCalc = fraction.divide(inputFrac);
+			}
+		}
 		
-		Fraction result = valueinCalc;
+		Fraction result = valueinCalc; //repeat for remaining string
 
 		return result;
 	}
