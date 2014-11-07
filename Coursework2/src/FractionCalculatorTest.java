41be99ca12e45729cdc8ce6f73daec66542b7d86
= <sassela@live.com>
2014-11-07 07:24:26 +0000

    Tests for whole number evaluation added

--- Coursework2/src/FractionCalculatorTest.java
+++ Coursework2/src/FractionCalculatorTest.java
@@ -15,6 +15,7 @@ public class FractionCalculatorTest {
 		assertEquals((new Fraction(1, 10)), FractionCalculator.evaluate((new Fraction(3, 10)), "+ -2/10"));
 		assertEquals((new Fraction(-3, 4)), FractionCalculator.evaluate((new Fraction(1, 6)), "- 11/12"));
 		assertEquals((new Fraction(2, 5)), FractionCalculator.evaluate((new Fraction(8, 10)), "/ 4/2"));
+		assertEquals((new Fraction(8, 5)), FractionCalculator.evaluate((new Fraction(8, 10)), "* 2"));
 		
 		//evaluates other single operation
 		assertEquals((new Fraction(2, 6)), FractionCalculator.evaluate((new Fraction(-2, 6)), "a"));
@@ -59,6 +60,7 @@ public class FractionCalculatorTest {
 		assertEquals((new Fraction(1, 10)), FractionCalculator.toFraction("2/20"));
 		assertEquals((new Fraction(-3, 4)), FractionCalculator.toFraction("-3/4"));
 		assertEquals((new Fraction(2, 5)), FractionCalculator.toFraction("-2/-5"));
+		assertEquals((new Fraction(10, 1)), FractionCalculator.toFraction("10"));
 	}
 
 }
