fcd7a914f9be12f00629d640a0c32925d04627be
= <sassela@live.com>
2014-11-09 13:38:41 +0000

    Refactoring: add wrapper methods

--- Coursework2/src/FractionCalculator.java
+++ Coursework2/src/FractionCalculator.java
@@ -14,10 +14,12 @@ import java.util.Scanner;
 public class FractionCalculator {
 	public static final String FRACTIONFORMAT = "(-?\\d+\\/-?\\d+)|(-?\\d+)";
 	public static final int MAXFRACELEMENTS = 2;
+
 	public static final String[] ABSOLUTE = {"a", "A", "abs"};
 	public static final String[] NEGATE = {"n", "N", "neg"};
 	public static final String[] CLEAR = {"c", "C", "clear"};
 	public static final String[] QUIT = {"q", "Q", "quit"};
+	public static final String[][] TEXTINPUT = {ABSOLUTE, NEGATE, CLEAR, QUIT};
 	public static final String[] SYMBOL = {"+", "-", "*", "/"};
 	public static final String SPACEDELIM = " ";
 	public static final String SLASHDELIM = "/";
@@ -52,38 +54,15 @@ public class FractionCalculator {
 	 
 		for (String inputSubstr : splitString) { 
 			
-			// evaluate other variables
-			if (Arrays.asList(ABSOLUTE).contains(inputSubstr)) {
-				valueInCalc = valueInCalc.getAbsVal();
-			} else if (Arrays.asList(NEGATE).contains(inputSubstr))  {
-				valueInCalc = valueInCalc.negate();
-			} else if (Arrays.asList(CLEAR).contains(inputSubstr))  {
-				valueInCalc = ZERO;
-			} else if (Arrays.asList(QUIT).contains(inputSubstr))  {
-				System.out.println("Goodbye");
-				System.exit(0);
-	        } else if (Arrays.asList(SYMBOL).contains(inputSubstr)) {
-				if(stored == EMPTY) {
-					stored = inputSubstr; 
-				} else {
-					System.out.println("Operator already stored, please enter a fraction");
-					break;
-				}
-			} else if(inputSubstr.matches(FRACTIONFORMAT)) {
-				Fraction inputFrac = toFraction(inputSubstr);
-				
-				//evaluate operation
-				if (stored.equals("+")) {
-				valueInCalc = valueInCalc.add(inputFrac);
-				} else if (stored.equals("-")) {
-					valueInCalc = valueInCalc.subtract(inputFrac);
-				} else if (stored.equals("*")) {
-					valueInCalc = valueInCalc.multiply(inputFrac);
-				} else if (stored.equals("/")) {
-					valueInCalc = valueInCalc.divide(inputFrac);
-				} else valueInCalc = inputFrac;
-				
+			if(inputSubstr.matches(FRACTIONFORMAT)) {
+				valueInCalc = evaluateFraction(valueInCalc, stored, inputSubstr);
 				stored = EMPTY; //once value operated on, clear stored symbol
+				
+			} else if (Arrays.asList(SYMBOL).contains(inputSubstr)) {
+				stored = evaluateSymbol(stored, inputSubstr);
+				
+			} else if (searchNestedArray(TEXTINPUT, inputSubstr)) {
+				valueInCalc = evaluateText(valueInCalc, inputSubstr);
 				
 			} else {
 				input = EMPTY;
@@ -117,7 +96,58 @@ public class FractionCalculator {
 		Fraction frac = new Fraction(num, denom);
 
 		return frac;
-
 	}
 
+	public static boolean searchNestedArray(String[][] nestedArray, String inputSubstr)  {
+		for (String[] other : nestedArray) {
+			if (Arrays.asList(other).contains(inputSubstr)) {
+			return true; 
+			}
+		} return false;
+	}
+	
+	public static Fraction evaluateText(Fraction valueInCalc, String inputSubstr) {
+		if (Arrays.asList(ABSOLUTE).contains(inputSubstr)) {
+			valueInCalc = valueInCalc.getAbsVal();
+		} else if (Arrays.asList(NEGATE).contains(inputSubstr))  {
+			valueInCalc = valueInCalc.negate();
+		} else if (Arrays.asList(CLEAR).contains(inputSubstr))  {
+			valueInCalc = ZERO;
+		} else if (Arrays.asList(QUIT).contains(inputSubstr))  {
+			valueInCalc = null;
+			quit();
+	    }
+		return valueInCalc;
+	}
+	
+	public static String evaluateSymbol(String stored, String inputSymbol) {
+		if(stored == EMPTY) {
+			return inputSymbol; 
+			} else {
+				System.out.println("Operator already stored, please enter a fraction");
+				return stored;
+			}
+	}
+	
+	public static Fraction evaluateFraction(Fraction valueInCalc, String inputSymbol, String inputSubstr) {
+		Fraction inputFrac = toFraction(inputSubstr);
+		
+		//evaluate operation
+		if (inputSymbol.equals("+")) {
+		valueInCalc = valueInCalc.add(inputFrac);
+		} else if (inputSymbol.equals("-")) {
+			valueInCalc = valueInCalc.subtract(inputFrac);
+		} else if (inputSymbol.equals("*")) {
+			valueInCalc = valueInCalc.multiply(inputFrac);
+		} else if (inputSymbol.equals("/")) {
+			valueInCalc = valueInCalc.divide(inputFrac);
+		} else valueInCalc = inputFrac;
+		
+		return valueInCalc;
+	}
+	
+	public static void quit() {
+		System.out.println("Goodbye");
+		System.exit(0);
+	}
 }
